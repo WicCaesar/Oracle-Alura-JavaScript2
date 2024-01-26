ff30f5a985d687141466233c9f81f384a419355e
Caesar Augustus <wiccaesar@hotmail.com>
2024-01-26 20:58:32 -0300

    Permite pressionar Enter além de clicar.
    Agora também é possível digitar o número e simplesmente apertar a tecla Enter, em vez de clicar no botão Chutar e novamente na caixa de texto.

--- app.js
+++ app.js
@@ -2,17 +2,19 @@
 
 let titulo = document.querySelector('h1');
 titulo.innerHTML = "Número secreto"
-
 let paragrafo = document.querySelector('p');
 paragrafo.innerHTML = `Escolha um número entre 1 e ${numeroMaximo}.`*/
 
+//* Troque o número abaixo para alterar a dificuldade.
 let numeroMaximo = 100;
 
+// Faz o sintetizador ler o nome do jogo em vez do texto estilizado.
 let TTS = false;
 responsiveVoice.setTextReplacements([{
 	searchvalue: ".NÚM3R0 5ECRE7O",
 	newvalue: "Número secreto"
 }]);
+
 function ativarTTS() {
 	if (TTS == false) {
 		TTS = true;
@@ -37,11 +39,13 @@ function mensagemInicial() {
 	// Substitui as primeiras quatro linhas do código. Economiza muitas linhas.
 	exibirTextoNaTela("h1", ".NÚM3R0 5ECRE7O");
 	exibirTextoNaTela("p", `Escolha um número entre 1 e ${numeroMaximo}.`)
-	console.log("Projeto desenvolvido por César Augusto do Nascimento,\n\
-para a formação da Oracle Next Education + Alura.\n\
-JavaScript: César Augusto do Nascimento. HTML e CSS base: Mônica Mazzochi Hillman.\n");
+	console.log("Projeto desenvolvido por César Augusto do Nascimento, \
+para a formação da Oracle Next Education + Alura - 2024.\n\
+JavaScript: César Augusto do Nascimento. \
+HTML e CSS base: Mônica Mazzochi Hillman.\n");
 }
 mensagemInicial();
+
 let tentativas = 0;
 let listaNumerosSecretos = [];
 
@@ -52,9 +56,11 @@ function gerarNumeroAleatorio() {
 	} else {
 		if (listaNumerosSecretos.length >= 4)
 			listaNumerosSecretos.shift();
-			// remove o primeiro elemento da lista, o número mais antigo.
-		listaNumerosSecretos.push(numeroSorteado);
-		//console.log(numeroSorteado);
+			// Remove o primeiro elemento da lista, o número mais antigo.
+		// Isso evita que o mesmo número seja sorteado várias vezes seguidas.
+	// Implementado apenas por ser parte do aprendizado. Não concordo com isso.
+		listaNumerosSecretos.push(numeroSorteado); // Inclui na lista.
+		//console.log(numeroSorteado); // Revela o número secreto no console.
 		return (numeroSorteado);
 	}
 }
@@ -79,8 +85,13 @@ function acertou() {
 	document.getElementById("reiniciar").removeAttribute("disabled");
 }
 
+// document.querySelector("input") é a caixa de texto onde o jogador palpita.
+// Aqui estou apenas criando uma variável para facilitar o uso abaixo, 
+// na função verificarChute() e na captura de sinal da tecla Enter.
+let input = document.querySelector("input");
+
 function verificarChute() {
-	let chute = parseInt(document.querySelector("input").value);
+	let chute = parseInt(input.value);
 	++tentativas;
 
 	if (chute == numeroSecreto)
@@ -96,6 +107,14 @@ function verificarChute() {
 	}
 	limparCampo();
 }
+
+// Faz a tecla Enter funcionar como um clique no botão Chutar.
+input.addEventListener("keyup", function(event) {
+	if (event.key == "Enter") {
+		event.preventDefault(); // Impede o comportamento padrão do Enter (como quebrar linhas em uma caixa de texto)
+		verificarChute();
+	}
+});
 
 function limparCampo() {
 	chute = document.querySelector("input");
